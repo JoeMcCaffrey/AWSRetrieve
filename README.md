5e5206815626e6ed448236907ce34479f3f88ba9
remy <remy@localhost.localdomain>
2017-02-02 20:58:39 -0500

    print out table and add readme

---
+++ README.md
@@ -0,0 +1,28 @@
+# Sonos Interview
+Using your favorite high-level language (Python, Ruby, Perl, or compiled), create a tool which uses AWS API access to list all EC2 instances in any single region, sorted by the value of a tag each instance has called ‘Owner’.  The script should display the results in an easy to read format which includes the instance id, tag value, instance type and launch time.  The script should work for any number of instances, and should display any instances without an Owner tag as type 'unknown' with the instance id, type and launch time.  
+
+Design the script so it could be used later to find tags with other names and output additional instance information by request.
+
+## Instructions
+
+This script takes 1 command line argument which is a .json file.
+
+The .json file should have the secret key, access key, ec2 region and 
+tag.
+
+### Description
+
+The script will open the .json file and call the getSortInstances function to connect to the ec2 region. 
+This function retrieves all the instances in that region and appends the instances with the matching tag into one list and non matching instances into another list. 
+The matching tag list is sorted by tag and the two list are concatinated into one.
+
+The printOut function is called to print out the instance data into a table.
+
+To change the tag that the script looks for, change the .json file with 
+another tag.
+
+To parse multiple tags, create an array of tags inside of the .json file
+and add a loop to the function getSortInstances function multiple times.
+
+To output more metadata about the instance, change the printOut function to index the instance.
+
--- regionRetrieve.py
+++ regionRetrieve.py
@@ -2,48 +2,67 @@
 
 import json
 import boto.ec2
-
+import sys
 
 def getSortInstances(region, tag):
     
+    # connect to amazon
     conn = boto.ec2.connect_to_region(region,
     aws_access_key_id=data["AccessKey"],
     aws_secret_access_key=data["SecretKey"])
-
+    
+    # get only instances for region
     instances = conn.get_only_instances(instance_ids=None, filters=None,\
         dry_run=False, max_results=None)
     
     owners = []
     nonOwners= []
+
+    # find out if each instance has tag and place into list
     for instance in instances:
         if tag in instance.tags:
             owners.append(instance)
         else:
             nonOwners.append(instance)
 
+    # sort owners list by tag
     owners.sort(key=lambda x:x.tags[tag])
+    # make one large with sorted first then unknowns
     mergedOwners = owners + nonOwners
-    ids= 'id'
-    print(mergedOwners[0].id)
+
     return mergedOwners
 
-
-def printOut(ownersList, field1, tag, field2, field3):
-
-    print("%10s|%10s|%10s|%10s" % (field1, tag, field2, field3))
+#change this function to print out more data about instance
+def printOut(ownersList, tag):
+    
+    # print out table header
+    template = "{0:19}|{1:7}|{2:13}|{3:12}"
+    print(template.format('ID', tag, 'Instance Type', 'Launch Time'))
+    #print("%10s%20s%20s%20s" % ('ID', tag, 'Instance Type', 'Launch Time '))
     
     for owner in ownersList:
+    # print out the sorted list then unknows list
         if tag in owner.tags:
-            print("%10s %10s %10s %10s" % (owner[field1], owner.tags[tag], \
-                owner[field2], owner[field3]))
+            print(template.format(owner.id, owner.tags[tag], \
+                owner.instance_type, owner.launch_time))
+             
+
         else:
-            print("%10s %10s %10s %10s" % (owner[field1], 'unknown', owner.field2, \
-                owner.field3))
+            print(template.format(owner.id, 'unknown', \
+                owner.instance_type, owner.launch_time))
 
+# open json file with script data as command line argument
+# so we can use this script with diffrent data
 
-with open('data.json') as data_file:
+if len(sys.argv) != 2:
+    print("This scirpt takes a json file as a command line argument")
+    sys.exit(1)
+
+with open(sys.argv[1]) as data_file:
     data = json.load(data_file)
 
+# call to sort instances by tag
 sortedTags = getSortInstances(data["region"], data["tag"])
 
-printOut(sortedTags, data["field1"],data["tag"],data["field2"], data["field3"])
+# print out results seperate from get and sort function
+printOut(sortedTags, data["tag"])
